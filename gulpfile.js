d0cfcf6f7d0d90dfa0d53c002e35912753b72689
Marco Antonio Ghiani <marcoantonio.ghiani01@gmail.com>
2019-08-16 11:50:38 +0200

    upgrade to v1.0.1

--- gulpfile.js
+++ gulpfile.js
@@ -1,63 +1,88 @@
-
 const gulp = require('gulp');
-const browserSync = require('browser-sync').create();
+const fs = require('fs');
+const del = require('del');
 const $ = require('gulp-load-plugins')();
+const browserSync = require('browser-sync').create();
 
-const publicDir = './public';
+function browserSyncTask () {
+  browserSync.init({
+    server: {baseDir: './public'}
+  });
+  gulp.watch([
+    './public/index.html',
+    './public/styles/style.css'
+  ]).on('change', browserSync.reload);
+}
 
-// Utility to ignore Node modules and Bower components
+// Utility to ignore unnecessary files
 // when generating the glob patterns array for gulp.src()
 function addDefSrcIgnore (srcArr) {
   return srcArr.concat([
+    '!**/REMOVE{,/**}',
     '!node_modules{,/**}',
-    `!${publicDir}/bower_components{,/**}`,
-
+    '!private{,/**}',
+    '!dist{,/**}',
     '!.git{,/**}',
     '!**/.DS_Store'
   ]);
 }
 
-gulp.task('default', function () {
-
-  browserSync.init({
-    server: {baseDir: publicDir}
-  });
-
-  gulp.watch([
-    publicDir + '/index.html',
-    publicDir + '/styles/*.css',
-    publicDir + '/bower_components/*',
-    publicDir + '/images/*'
-  ]).on('change', browserSync.reload);
-
-});
-
-// Lint all files
-gulp.task('lint', ['lint-js', 'lint-html', 'lint-css']);
-
 // JavaScript and JSON linter
-gulp.task('lint-js', function () {
+function lintJs () {
   return gulp.src(addDefSrcIgnore(['**/*.js', '**/*.json']), {dot: true})
     .pipe($.eslint({dotfiles: true}))
     .pipe($.eslint.format())
     .pipe($.eslint.failAfterError());
-});
+}
 
 // HTML linter
-gulp.task('lint-html', function () {
-  return gulp.src([`${publicDir}/*.html`])
-    .pipe($.htmlLint({htmllintrc: '.htmllintrc.json'}))
-    .pipe($.htmlLint.format())
-    .pipe($.htmlLint.failAfterError());
-});
+function lintHtml () {
+  return gulp.src(addDefSrcIgnore(['**/*.html']))
+    .pipe($.htmllint({config: '.htmllintrc.json', failOnError: true}));
+}
 
 // CSS linter
-gulp.task('lint-css', function () {
-  return gulp.src([`${publicDir}/styles/*.css`])
+function lintCss () {
+  return gulp.src(addDefSrcIgnore(['**/*.css']))
     .pipe($.stylelint({
       failAfterError: true,
-      reporters: [
-        {formatter: 'string', console: true}
-      ]
+      reporters: [{formatter: 'string', console: true}]
     }));
-});
+}
+
+// Remove solutions from exercises
+function removeSolutions () {
+  del.sync('dist');
+  return gulp.src(addDefSrcIgnore(['**']), {dot: true})
+    .pipe($.replace(/^\s*(\/\/|<!--|\/\*)\s*REMOVE-START[\s\S]*?REMOVE-END\s*(\*\/|-->)?\s*$/gm, ''))
+    .pipe(gulp.dest('dist'));
+}
+
+// Prepare for distribution to students
+function updateConfigForSlave (done) {
+  let npmConfig = require('./package.json');
+  npmConfig = JSON.stringify(npmConfig, null, 2).replace(/-master/g, '');
+  fs.writeFileSync('dist/package.json', npmConfig);
+  const esLintConfig = require('./.eslintrc.json');
+  esLintConfig.rules['no-undef'] = 'off';
+  esLintConfig.rules['no-unused-vars'] = 'off';
+  fs.writeFileSync('dist/.eslintrc.json', JSON.stringify(esLintConfig, null, 2));
+
+  done();
+}
+
+// Lint all files
+exports.lint = gulp.parallel(
+  lintJs,
+  lintHtml,
+  lintCss
+);
+
+// Prepare for distribution to students
+exports.dist = gulp.series(
+  removeSolutions,
+  updateConfigForSlave
+);
+
+// Open browser sync and watch files
+exports.default = browserSyncTask;
